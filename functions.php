0f3535fa07ef7143a0e66576ebfee9e10e0eefbd
KUATOproject <KUATOimac@admin.lehrer.ruhr-uni-bochum.de>
2016-02-16 14:04:06 +0100

    - woocommerce shop eingebaut

--- functions.php
+++ functions.php
@@ -136,3 +136,8 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
